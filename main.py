bbb687e8a6d54c34b79c17f1f28ce0e10b95af54
Mykola-programer360 <114693260+Mykola-programer360@users.noreply.github.com>
2022-11-05 14:00:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+a = "Hello, Python!"
+print(a)
+
+
+
+a = int(input('enter a:'))
+b = float(input('enter b:'))
+c = id(b)
+print(a+b)
+
+
+
+a = input("Введіть ім'я:")
+b = input("Введіть прізвище:")
+c = input("Введіть вік:")
+print(f"Привіт {a} {b}!!!Тобі зараз {c} років")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
